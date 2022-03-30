6a73d0a940e9836674d33c416664d95951d24e3b
Libin91 <airani91@gmail.com>
2022-03-30 21:45:25 +0100

    changes

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function yourQuestion() {
-    let answerOption = ['yes','no', 'maybe', 'might be', 'never', 'dumb question tbh'];
-    let result = answerOption[Math.floor(Math.random()*answerOption.length)];
-   
-    let userAnswer = document.getElementById("QUESTION").value;
-    document.getElementById("magic8ballanswer").innerHTML ="The ball say:" + result;
-    document.getElementById("QUESTION").value = "";
+import {answers} from "./modules/answers.js";
 
-};
+let audio = new Audio('Tada-sound.mp3')
+let result = answers[Math.floor(Math.random()*answers.length)];
+let submitbutton = document.querySelector("#fortunebutton");
+let resetTheQuestion = document.querySelector("#reset");
+let inpuT = document.getElementById('QUESTION');
+let magicAnswer = document.getElementById("magic8ballanswer");
+// let btn = document.getElementById('fortunebutton');
+
+inpuT.addEventListener("input", function(){
+  submitbutton.disabled = (this.value === '');
+})
+
+ function yourQuestion()  {
+    audio.play();
+    magicAnswer.innerHTML ="The ball says: " + result;
+    inpuT.value = "";
+     
+ }
 
 function resetQuestion(){
     window.location.reload();
 }
+
+submitbutton.addEventListener("click", yourQuestion);
+resetTheQuestion.addEventListener("click", resetQuestion);
